4337612cc8d9a76f99226981fbe1ee9410db77b6
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-24 20:05:49 +0500

    Refactor currencyConverter function

--- src/api/PostApi.jsx
+++ src/api/PostApi.jsx
@@ -5,7 +5,8 @@ const api = axios.create({
   baseURL: `https://v6.exchangerate-api.com/v6/${Api_Key}`,
 });
 
-// we need to crate a  get request
-export const currencyConverter = (fromCurrency, toCurrency, amount) => {
+const currencyConverter = (fromCurrency, toCurrency, amount) => {
   return api.get(`/pair/${fromCurrency}/${toCurrency}/${amount}`);
 };
+
+export default currencyConverter;
